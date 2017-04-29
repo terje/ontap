4dcf0f368a04bdd6be00e530eb4eb745f876aea8
Terje Tjervaag <terje@tjervaag.com>
2017-04-29 20:55:50 +0200

    Fetch taps

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
+import fetchStream from 'fetch-readablestream';
 import logo from './logo.svg';
 import './App.css';
+import Tap from './Tap'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { taps: [] }
+
+    fetch('http://localhost:5000/api/taps')
+      .then(response => {
+        if (response.ok) {
+          return response.json()
+        }
+        console.log("Error", response);
+      })   
+      .then(json => this.setState(Object.assign({}, this.state, { taps: json })))
+  }
+
   render() {
+    let taps = null;
+    if (this.state.taps) {
+        taps = this.state.taps.map(tap => <Tap key={tap} name={tap} />)
+    }
     return (
       <div className="App">
         <div className="App-header">
@@ -11,6 +32,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
+          {taps}
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
---
+++ src/Tap.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const Tap = props => {
+  return (
+    <div>
+      <strong>Tap: </strong> {props.name}
+    </div>
+  );
+}
+
+export default Tap
